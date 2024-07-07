d7ecc50f5918c59f140ca9c04671908738b0e61f
margi1an <shermatovelbek69@gmail.com>
2024-07-07 22:52:54 +0300

    uyga vazifa tugatildi

--- src/app/singleProduct/[id]/page.jsx
+++ src/app/singleProduct/[id]/page.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import ronaldo from '../../../../public/ronaldo.jpg';
+import ronaldo from "../../../../public/ronaldo.jpg";
 
 const getData = async (id) => {
   const request = await fetch(`https://dummyjson.com/products/` + id);
@@ -12,24 +12,32 @@ const getData = async (id) => {
 
 async function Product({ params }) {
   const product = await getData(params.id);
-  
+  console.log(product);
+
+  if (!product) {
+    return (
+      <div className="align-elements ml-48 mt-48">
+        <span className="loading ml-96 pl-10 loading-dots w-32 loading-lg"></span>
+      </div>
+    );
+  }
   return (
     <div className="align-elements mt-16">
-        <Link href={'/products'}>Product Page Back</Link>
+      <Link href={"/products"}>
+        <button className="btn btn-primary mb-4">Product Page Back ✋</button>
+      </Link>
       <div className="card lg:card-side bg-slate-700 text-slate-200  glass shadow-xl">
+        <div  className="rounded-lg">
         <figure>
-        <Image
-        className="rounded-lg"
-            src={product.images}
-            alt={product.title} 
-            width={1000} 
-            height={1000}
-          />
+          <img src={product.thumbnail} alt={product.title} className="h-96 " />
         </figure>
+        </div>
         <div className="card-body">
           <h2 className="mb-4 card-title">{product.title}</h2>
-          <p className = 'mb-4 font-semibold'>{product.description}</p>
-          <p className="font-bold">Price : {product.price}</p>
+          <p className="mb-4 font-semibold">{product.description}</p>
+          <p className="font-bold">Price : {product.price} $</p>
+          <p className="font-semibold">Category : {product.category}</p>
+          <p className="font-semibold">Brand : {product.brand}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Add to Bag</button>
           </div>
